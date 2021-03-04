e473df173a5da18342143d03705207271bb45b69
Kubrick G <513776985@qq.com>
2021-03-04 14:24:08 +0800

    tem save

--- framework/Source/iOS/MoviePlayer.swift
+++ framework/Source/iOS/MoviePlayer.swift
@@ -56,7 +56,11 @@ public class MoviePlayer: AVQueuePlayer, ImageSource {
     public var dropFrameBeforeTime: CMTime?
     public var playrate: Float = 1.0
     public var assetDuration: CMTime {
-        return asset?.duration ?? .zero
+        if asset?.statusOfValue(forKey: "duration", error: nil) == .loaded {
+            return asset?.duration ?? .zero
+        } else {
+            return .zero
+        }
     }
     public var isReadyToPlay: Bool {
         return status == .readyToPlay
@@ -288,7 +292,7 @@ public class MoviePlayer: AVQueuePlayer, ImageSource {
         }
         isPlaying = true
         isProcessing = false
-        print("[MoviePlayer] start duration:\(String(describing: asset?.duration.seconds)) items:\(items())")
+//        print("[MoviePlayer] start duration:\(String(describing: asset?.duration.seconds)) items:\(items())")
         _setupDisplayLinkIfNeeded()
         _resetTimeObservers()
         didNotifyEndedItem = nil
@@ -562,8 +566,8 @@ private extension MoviePlayer {
     }
     
     func playerItemStatusDidChange(_ playerItem: AVPlayerItem) {
-        debugPrint("[MoviePlayer] PlayerItem status change to:\(playerItem.status.rawValue) asset:\(playerItem.asset)")
-        if playerItem == currentItem {
+        debugPrint("[MoviePlayer] PlayerItem status change to:\(playerItem.status.rawValue) asset:\(playerItem.asset), error: \(playerItem.error)")
+        if playerItem == currentItem && playerItem.status == .readyToPlay {
             resumeIfNeeded()
         }
     }
